149c4f919313f46e50ba8332a6bd28a08442e34d
James Lamb <jlamb@ucsb.edu>
2024-02-01 16:03:41 -0800

    Ready to test.

---
+++ create_openxy_inputs.py
@@ -0,0 +1,129 @@
+# Script to create an sub-region ANG file and a .txt grain file from the full ang and grain files
+import os
+import numpy as np
+import ebsd_pattern
+from skimage import io
+from tqdm.auto import tqdm
+
+### USER INPUTS ###
+# folder = "E:/James/"
+# fname = "DED_CoNi90"
+folder = "/Users/jameslamb/Downloads/"
+fname = "DED_CoNi90"
+save_extension = "_roi"
+roi_start = (0, 0)
+roi_dims = (100, 100)
+number_of_grain_columns = 11
+### END USER INPUTS ###
+
+ang_path = folder + fname + ".ang"
+grain_path = folder + fname + ".txt"
+up2_path = folder + fname + ".up2"
+up2_save_folder = folder + "patterns/"
+ang_save_path = folder + fname + save_extension + ".ang"
+grain_save_path = folder + fname + save_extension + ".txt"
+
+# Need to grab the header from the ang file and grab the scan dimensions/resolution
+print("Parsing ANG file...")
+with open(ang_path, 'r') as f:
+    for i, l in enumerate(f.readlines()):
+        if l[0] != "#":
+            header_end = i
+            break
+        elif l[:13] == "# NCOLS_EVEN:":
+            n_cols = int(float(l.split(":")[1].replace("\n", "")))
+        elif l[:8] == "# NROWS:":
+            n_rows = int(float(l.split(":")[1].replace("\n", "")))
+        elif l[:8] == "# XSTEP:":
+            x_step = float(l.split(":")[1].replace("\n", ""))
+        elif l[:8] == "# YSTEP:":
+            y_step = float(l.split(":")[1].replace("\n", ""))
+
+header = open(ang_path).readlines()[:header_end]
+data = np.genfromtxt(ang_path, skip_header=header_end)
+data = data.reshape(n_rows, n_cols, data.shape[1])
+
+print("  Number of header lines: ", header_end)
+print("  Scan dimensions: ", n_rows, n_cols)
+print("  Scan resolution: ", y_step, x_step)
+print("  Data shape: ", data.shape)
+print("  x range: ", data[0, 0, 3], data[-1, -1, 3])
+print("  y range: ", data[0, 0, 4], data[-1, -1, 4])
+
+roi_data = data[roi_start[0]:roi_start[0]+roi_dims[0], roi_start[1]:roi_start[1]+roi_dims[1], :]
+roi_y, roi_x = np.indices(roi_dims, dtype=float)
+roi_x *= x_step
+roi_y *= y_step
+roi_data[:, :, 3] = roi_x
+roi_data[:, :, 4] = roi_y
+
+print("  ROI data shape: ", roi_data.shape)
+print("  ROI x range: ", roi_data[0, 0, 3], roi_data[-1, -1, 3])
+print("  ROI y range: ", roi_data[0, 0, 4], roi_data[-1, -1, 4])
+
+print("  Writing new ANG file...")
+roi_data = roi_data.reshape(-1, roi_data.shape[2])
+with open(ang_save_path, 'w') as f:
+    for l in header:
+        f.write(l)
+    for l in roi_data:
+        line = "  {:.5f}   {:.5f}   {:.5f}      {:.5f}      {:.5f}    {:.1f}  {:.3f}  {:.0f}      {:.0f} {:.3f}\n".format(*l)
+        f.write(line)
+
+print("Parsing grain file...")
+with open(grain_path, 'r') as f:
+    for i, l in enumerate(f.readlines()):
+        if l[0] != "#":
+            header_end = i
+            break
+
+header = open(grain_path).readlines()[:header_end]
+data = np.genfromtxt(grain_path, skip_header=header_end, dtype=str)
+phase = data[:, number_of_grain_columns-1:]
+if phase.shape[1] > 1:
+    phase = np.array(["_".join(p) for p in phase])
+data = data[:, :number_of_grain_columns-1].astype(float)
+phase = phase.reshape(n_rows, n_cols)
+data = data.reshape(n_rows, n_cols, number_of_grain_columns-1)
+
+print("  Number of header lines: ", header_end)
+print("  Data shape: ", data.shape)
+print("  Phase shape: ", phase.shape)
+print("  x range: ", data[0, 0, 3], data[-1, -1, 3])
+print("  y range: ", data[0, 0, 4], data[-1, -1, 4])
+
+roi_data = data[roi_start[0]:roi_start[0]+roi_dims[0], roi_start[1]:roi_start[1]+roi_dims[1], :]
+roi_phase = phase[roi_start[0]:roi_start[0]+roi_dims[0], roi_start[1]:roi_start[1]+roi_dims[1]]
+roi_data[:, :, 3] = roi_x
+roi_data[:, :, 4] = roi_y
+
+print("  ROI data shape: ", roi_data.shape)
+print("  ROI phase shape: ", roi_phase.shape)
+print("  ROI x range: ", roi_data[0, 0, 3], roi_data[-1, -1, 3])
+print("  ROI y range: ", roi_data[0, 0, 4], roi_data[-1, -1, 4])
+
+print("  Writing new grain file...")
+roi_data = roi_data.reshape(-1, roi_data.shape[2])
+roi_phase = roi_phase.reshape(-1)
+with open(grain_save_path, 'w') as f:
+    for l in header:
+        f.write(l)
+    for l, p in zip(roi_data, roi_phase):
+        line = "  {:.5f}   {:.5f}   {:.5f}      {:.5f}      {:.5f}    {:.1f}  {:.3f}  {:.0f}   {:.0f}  {:.3f}  {}\n".format(*l, p)
+        f.write(line)
+
+print("Orientation and Grain files created. Exporting patterns...")
+ebsd_obj = ebsd_pattern.get_pattern_file_obj(up2_path)
+ebsd_obj.read_header()
+print("  Number of patterns: ", ebsd_obj.nPatterns)
+print("  Pattern dimensions: ", ebsd_obj.patternH, ebsd_obj.patternW)
+
+ii = np.arange(n_rows * n_cols).reshape(n_rows, n_cols)
+ii = ii[roi_start[0]:roi_start[0]+roi_dims[0], roi_start[1]:roi_start[1]+roi_dims[1]].flatten()
+print("  Number of patterns in ROI: ", ii.size)
+
+os.mkdir(up2_save_folder, exist_ok=True)
+for i in tqdm(ii, desc="Exporting patterns", unit="pats"):
+    pat = np.squeee(ebsd_obj.pat_reader(i, 1))
+    pat = np.around(255 * (pat - pat.min()) / (pat.max() - pat.min())).astype(np.uint8)
+    io.imsave(f"{up2_save_folder}{i}.png", pat)
