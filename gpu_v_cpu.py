fdbe6707ec6e6bf494af86d71dc1e0bf65f8f4e0
James Lamb <jlamb@ucsb.edu>
2024-05-21 15:55:26 -0700

    Trying out some batch functionality

--- gpu_v_cpu.py
+++ gpu_v_cpu.py
@@ -11,27 +11,60 @@ if torch.cuda.is_available():
 else:
     device = torch.device('cpu')
 
-def W(p) -> np.ndarray:
-    return np.array([[1 + p[0],     p[1], p[2]],
-                     [    p[3], 1 + p[4], p[5]],
-                     [    p[6],     p[7],    1]])
+def W_vectorized(p) -> np.ndarray:
+    """Convert homographies into a shape function.
+    Assumes p is a (Nx8) array."""
+    in_shape = p.shape[:-1]
+    _0 = np.zeros(in_shape + (1,))
+    return np.append(p, _0, axis=-1).reshape(in_shape + (3, 3,)) + np.eye(3)[None, ...]
 
-def W_gpu(p) -> torch.Tensor:
-    return torch.tensor([[1 + p[0],     p[1], p[2]],
-                         [    p[3], 1 + p[4], p[5]],
-                         [    p[6],     p[7],    1]])
+def W(p) -> np.ndarray:
+    """Convert homographies into a shape function.
+    Assumes p is a (Nx8) array."""
+    return np.append(p, 0).reshape(3, 3) + np.eye(3)
+
+def normalize_vectorized(img):
+    """Zero mean, unit variance normalization of an image.
+    Assumes the images has been flattened to a 2D array (N, M),
+    where N is the number of images and M is the number of pixels."""
+    img_bar = img.mean(axis=-1)[...,None]#, keepdims=True)
+    dimg_tilde = np.sqrt(((img - img_bar)**2).sum(axis=-1)[..., None])#, keepdims=True))
+    return (img - img_bar) / dimg_tilde
+
+def dp_norm_vectorized(dp, xi) -> float:
+    """Compute the norm of the delta p vector.
+    Assumes dp is a (Nx8) array and xi is a (2,M) array."""
+    xi1max = xi[0].max()
+    xi2max = xi[1].max()
+    ximax = np.array([[xi1max, xi2max]])
+    dp_i0 = np.square(dp[:, 0:2] * ximax).sum(axis=-1)
+    dp_i1 = np.square(dp[:, 3:5] * ximax).sum(axis=-1)
+    dp_i2 = np.square(dp[:, 6:8] * ximax).sum(axis=-1)
+    out = np.sqrt(dp_i0 + dp_i1 + dp_i2 + np.square(dp[:, 2]) + np.square(dp[:, 5]))
+    return out
 
 def normalize(img):
+    """Zero-mean normalize an image with unit standard deviation.
+    Note that the standard deviation is multiplied by the number of pixels minus one.
+    Args:
+        img (np.ndarray): The image to normalize.
+    Returns:
+        np.ndarray: The normalized image."""
     img_bar = img.mean()
     dimg_tilde = np.sqrt(((img - img_bar)**2).sum())
-    return (img - img_bar) / dimg_tilde
-
-def normalize_gpu(img):
-    img_bar = img.mean()
-    dimg_tilde = torch.sqrt(((img - img_bar)**2).sum())
-    return (img - img_bar) / dimg_tilde
+    out = (img - img_bar) / dimg_tilde
+    return out
 
 def dp_norm(dp, xi) -> float:
+    """Compute the norm of the deformation increment.
+    This is essentially a modified form of a homography magnitude.
+
+    Args:
+        dp (np.ndarray): The deformation increment. Shape is (8,).
+        xi (np.ndarray): The subset coordinates. Shape is (2, N).
+
+    Returns:
+        float: The norm of the deformation increment."""
     xi1max = xi[0].max()
     xi2max = xi[1].max()
     ximax = np.array([xi1max, xi2max])
@@ -41,43 +74,36 @@ def dp_norm(dp, xi) -> float:
     out = np.sqrt((dp_i0**2).sum() + (dp_i1**2).sum() + (dp_i2**2).sum() + (dp[2]**2 + dp[5]**2))
     return out
 
-def dp_norm_gpu(dp, xi) -> torch.Tensor:
-    xi1max = xi[0].max()
-    xi2max = xi[1].max()
-    ximax = torch.tensor([xi1max, xi2max])
-    dp_i0 = torch.tensor([dp[0], dp[1]]) * ximax
-    dp_i1 = torch.tensor([dp[3], dp[4]]) * ximax
-    dp_i2 = torch.tensor([dp[6], dp[7]]) * ximax
-    out = torch.sqrt((dp_i0**2).sum() + (dp_i1**2).sum() + (dp_i2**2).sum() + (dp[2]**2 + dp[5]**2))
-    return out
-
-def main_cpu(r, t, p, NablaR_dot_Jac, H, r_zmsv, xi):
-    t = normalize(t)
-    e = r - t
-    dC_IC_ZNSSD = 2 / r_zmsv * np.matmul(e, NablaR_dot_Jac.T)  # 8x1
+def main(r, t, p, NablaR_dot_Jac, H, r_zmsv, xi):
     c, lower = linalg.cho_factor(H)
-    dp = linalg.cho_solve((c, lower), -dC_IC_ZNSSD.reshape(-1, 1))[:, 0]
-    norm = dp_norm(dp, xi)
-    Wp = W(p).dot(np.linalg.inv(W(dp)))
-    Wp = (Wp / Wp[2, 2]) - np.eye(3)
-    p = Wp.flatten()[:8]
+    for i in range(t.shape[0]):
+        for j in range(10):
+            e = r - normalize(t[i])
+            dC_IC_ZNSSD = 2 / r_zmsv * np.matmul(e, NablaR_dot_Jac.T)  # 8x1
+            dp = linalg.cho_solve((c, lower), -dC_IC_ZNSSD.reshape(-1, 1))[:, 0]
+            norm = dp_norm(dp, xi)
+            Wp = W(p[i])
+            Wdp = W(dp)
+            Wpdp = Wp.dot(np.linalg.inv(Wdp))
+            p_out = ((Wpdp / Wpdp[2, 2]) - np.eye(3)).flatten()[:8]
 
-def main_gpu(r, t, p, NablaR_dot_Jac, H, r_zmsv, xi):
-    t = torch.tensor(t).to(device)
-    t = normalize_gpu(t)
-    e = r - t
-    dC_IC_ZNSSD = 2 / r_zmsv * torch.matmul(e, NablaR_dot_Jac.T)  # 8x1
-    L = torch.linalg.cholesky(H)
-    dp = torch.cholesky_solve(-dC_IC_ZNSSD.reshape(-1, 1), L)[:, 0]
-    norm = dp_norm_gpu(dp, xi)
-    Wp = W_gpu(p).matmul(torch.linalg.inv(W_gpu(dp)))
-    Wp = (Wp / Wp[2, 2]) - torch.eye(3)
-    p = Wp.flatten()[:8]
+def main_vectorized(r, t, p, NablaR_dot_Jac, H, r_zmsv, xi):
+    for j in range(10):
+        e = r - normalize(t)
+        dC_IC_ZNSSD = 2 / r_zmsv * np.einsum('ij,kj->ik', e, NablaR_dot_Jac)  # 8x1
+        c, lower = linalg.cho_factor(H)
+        dp = linalg.cho_solve((c, lower), -dC_IC_ZNSSD.T).T
+        norm = dp_norm_vectorized(dp, xi)
+        Wp = W_vectorized(p)
+        Wdp = W_vectorized(dp)
+        Wpdp = Wp @ np.linalg.inv(Wdp)
+        p = ((Wpdp / Wpdp[:, 2, 2][:, None, None]) - np.eye(3)[None, ...])
+        p = p.reshape(-1, 9)[:, :8]
+
 
 path = "/Users/jameslamb/Library/Mobile Documents/com~apple~CloudDocs/Downloads/CoNi67_Pattern.tif"
 r = io.imread(path)
-r = normalize(r)
-t = normalize(r)
+t = r
 
 x = np.arange(r.shape[1]) - 512
 y = np.arange(r.shape[0]) - 512
@@ -86,23 +112,23 @@ xi = np.array([Y.flatten(), X.flatten()]).astype(np.float32)
 r_zmsv = np.sqrt(((r - r.mean())**2).sum()).astype(np.float32)
 NablaR_dot_Jac = np.random.rand(8, r.shape[0]*r.shape[1]).astype(np.float32)
 H = 2 / r_zmsv**2 * NablaR_dot_Jac.dot(NablaR_dot_Jac.T)
-p = np.zeros(8).astype(np.float32)
+p = np.random.rand(8).astype(np.float32)
 r = r.flatten().astype(np.float32)
 t = t.flatten().astype(np.float32)
 
-xi_gpu = torch.tensor(xi).to(device)
-r_gpu = torch.tensor(r).to(device)
-t_gpu = torch.tensor(t).to(device)
-NablaR_dot_Jac_gpu = torch.tensor(NablaR_dot_Jac).to(device)
-H_gpu = torch.tensor(H).to(device)
-p_gpu = torch.tensor(p).to(device)
+t_v = np.repeat(r[None, :], 10, axis=0)
+p_v = np.repeat(p[None, :], 10, axis=0)
+p_out = main_vectorized(r, t_v, p_v, NablaR_dot_Jac, H, r_zmsv, xi)
 
-iterations = 1000
+t0 = timeit.timeit(lambda: main(r, t_v, p_v, NablaR_dot_Jac, H, r_zmsv, xi), number=100) / 1000
+print(f"Time: {t0} seconds/solution")
+t1 = timeit.timeit(lambda: main_vectorized(r, t_v, p_v, NablaR_dot_Jac, H, r_zmsv, xi), number=100) / 1000
+print(f"Time: {t1} seconds/solution")
+# dp_norm_speedup = 93.5
+exit()
+
+iterations = 10000
 
 print("CPU")
 tcpu = timeit.timeit(lambda: main_cpu(r, t, p, NablaR_dot_Jac, H, r_zmsv, xi), number=iterations)
-print(f"Time: {tcpu/iterations} seconds/iteration")
-
-print("GPU")
-tcpu = timeit.timeit(lambda: main_gpu(r_gpu, t_gpu, p_gpu, NablaR_dot_Jac_gpu, H_gpu, r_zmsv, xi_gpu), number=iterations)
 print(f"Time: {tcpu/iterations} seconds/iteration")
